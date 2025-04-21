e1f9f41cf65e4a78bfe041a64917030a77646484
yuripnp <yuripnp1992@gmail.com>
2025-04-21 11:31:31 -0300

    casting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        Paciente paciente = new Paciente();
+        double peso = 70.0;
+        double altura = 1.75;
+        int anos = 20;
+        int alturaEmCentimetros = (int) altura * 100;
+        double anosEMeses = anos * 12;
     }
 }
---
+++ src/Paciente.java
@@ -0,0 +1,6 @@
+public class Paciente {
+    public String nome;
+    public double peso;
+    public double altura;
+    public char sexo;
+}
